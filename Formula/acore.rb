f2dafa9b1ae758e47a3eaf856b69b7f35e8ff786
YashMakan <contact@yashmakan.com>
2026-02-13 06:19:04 +0530

    Add v0.1.0 formulas

--- Formula/acore.rb
+++ Formula/acore.rb
@@ -1,11 +1,15 @@
 class Acore < Formula
-  desc "Acore CLI"
+  desc "Acore evaluation engine for Axiom"
   homepage "https://github.com/AxiomCore/cli"
-  url "https://github.com/AxiomCore/cli/releases/download/v0.1.0/jpkl-macos-arm64.tar.gz"
-  sha256 "6ef58ea34f98cbd9d76a094727fd1dedd625ae37d9e48f3274333fba1a294e2f"
+  url "https://github.com/AxiomCore/cli/releases/download/v0.1.0/acore-macos-arm64.tar.gz"
+  sha256 "de99122732308ae221e1a7f39ae8b2be50550fbfded30415c0ff985aee414440"
   version "0.1.0"
 
   def install
     bin.install "acore"
+  end
+
+  test do
+    assert_match "Pkl", shell_output("#{bin}/acore --version")
   end
 end
--- Formula/axiom.rb
+++ Formula/axiom.rb
@@ -2,12 +2,16 @@ class Axiom < Formula
   desc "Axiom CLI"
   homepage "https://github.com/AxiomCore/cli"
   url "https://github.com/AxiomCore/cli/releases/download/v0.1.0/axiom-macos-arm64.tar.gz"
-  sha256 "c3cd186527cda01ec8ad95b28d1dba94a843c491053e1d0c175b4927c9a5cf94"
+  sha256 "5acc198fe3cc8eaafd290f1f6a9a05cb71c4071ee1a97a78e7a0f29c376f4b9e"
   version "0.1.0"
 
   depends_on "acore"
 
   def install
     bin.install "axiom"
+  end
+
+  test do
+    assert_match "Usage", shell_output("#{bin}/axiom --help")
   end
 end
