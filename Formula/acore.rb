a82ea932be0ebc640fd2914591416feca626043a
YashMakan <contact@yashmakan.com>
2026-02-13 06:20:27 +0530

    Add v0.0.1 formulas

--- Formula/acore.rb
+++ Formula/acore.rb
@@ -1,9 +1,9 @@
 class Acore < Formula
   desc "Acore evaluation engine for Axiom"
   homepage "https://github.com/AxiomCore/cli"
-  url "https://github.com/AxiomCore/cli/releases/download/v0.1.0/acore-macos-arm64.tar.gz"
+  url "https://github.com/AxiomCore/cli/releases/download/v0.0.1/acore-macos-arm64.tar.gz"
   sha256 "de99122732308ae221e1a7f39ae8b2be50550fbfded30415c0ff985aee414440"
-  version "0.1.0"
+  version "0.0.1"
 
   def install
     bin.install "acore"
--- Formula/axiom.rb
+++ Formula/axiom.rb
@@ -1,9 +1,9 @@
 class Axiom < Formula
   desc "Axiom CLI"
   homepage "https://github.com/AxiomCore/cli"
-  url "https://github.com/AxiomCore/cli/releases/download/v0.1.0/axiom-macos-arm64.tar.gz"
+  url "https://github.com/AxiomCore/cli/releases/download/v0.0.1/axiom-macos-arm64.tar.gz"
   sha256 "5acc198fe3cc8eaafd290f1f6a9a05cb71c4071ee1a97a78e7a0f29c376f4b9e"
-  version "0.1.0"
+  version "0.0.1"
 
   depends_on "acore"
 
