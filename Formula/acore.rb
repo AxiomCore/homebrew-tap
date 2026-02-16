b9c779c1e91ebfb6f18a1f4a9682c071a6da67b2
YashMakan <contact@yashmakan.com>
2026-02-16 17:22:12 +0530

    Updating to v0.0.35

--- Formula/acore.rb
+++ Formula/acore.rb
@@ -1,9 +1,9 @@
 class Acore < Formula
   desc "Acore evaluation engine for Axiom"
   homepage "https://github.com/AxiomCore/cli"
-  url "https://github.com/AxiomCore/cli/releases/download/v0.0.34/acore-macos-arm64.tar.gz"
-  sha256 "be82f3f7c057b4f2af2d088b09b1ee373647bd49d6b2992774920f7fd3b68742"
-  version "0.0.34"
+  url "https://github.com/AxiomCore/cli/releases/download/v0.0.35/acore-macos-arm64.tar.gz"
+  sha256 "57c581ba9188d289cee5bc5d1674d6842e1db02ad8b21ebb912c3600f16952be"
+  version "0.0.35"
 
   def install
     chmod 0755, "acore" 
--- Formula/axiom.rb
+++ Formula/axiom.rb
@@ -1,9 +1,9 @@
 class Axiom < Formula
   desc "Axiom CLI"
   homepage "https://github.com/AxiomCore/cli"
-  url "https://github.com/AxiomCore/cli/releases/download/v0.0.34/axiom-macos-arm64.tar.gz"
-  sha256 "5d5f34350de88fa561e7968bc3868488deb7f070112e749c6f8fe3dc19039ea5"
-  version "0.0.34"
+  url "https://github.com/AxiomCore/cli/releases/download/v0.0.35/axiom-macos-arm64.tar.gz"
+  sha256 "49ea74392cf1ed1e7cf4b6d0001b134f4ca79127d8bb023045fc18abd104fe47"
+  version "0.0.35"
 
   depends_on "acore"
 
