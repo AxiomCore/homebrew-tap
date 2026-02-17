921eccfd129af816c349c319358c771b6355e446
YashMakan <contact@yashmakan.com>
2026-02-17 18:41:58 +0530

    Updating to v0.0.57

--- Formula/axiom.rb
+++ Formula/axiom.rb
@@ -1,9 +1,9 @@
 class Axiom < Formula
   desc "Axiom CLI"
   homepage "https://github.com/AxiomCore/cli"
-  url "https://github.com/AxiomCore/cli/releases/download/v0.0.55/axiom-macos-arm64.tar.gz"
-  sha256 "25e64bd9b433a98745bcedbf54ce5fbc5e18212c4512d9c7a733a711506196b7"
-  version "0.0.55"
+  url "https://github.com/AxiomCore/cli/releases/download/v0.0.57/axiom-macos-arm64.tar.gz"
+  sha256 "984e619eac932663edd22f2c4fa9ba0c4946d1c4025beb3ad6c9908e690ed41d"
+  version "0.0.57"
 
   depends_on "acore"
 
