40b6db182e14d534f30a7f8f311b890406f01492
YashMakan <contact@yashmakan.com>
2026-04-22 16:19:10 +0530

    Update Axiom to 0.1.93

--- Formula/axiom.rb
+++ Formula/axiom.rb
@@ -1,9 +1,9 @@
 class Axiom < Formula
   desc "Axiom CLI - Unified Configuration and API SDK Generator"
   homepage "https://github.com/AxiomCore/AxiomCore"
-  url "https://github.com/AxiomCore/AxiomCore/releases/download/v0.1.93/axiom-macos-arm64.tar.gz"
-  sha256 "2e3436a5aa06d601b3f2e5235fde9f46e16d9470c5dbadd1e7d07d63b149c389"
-  version "v0.1.93"
+  url "https://github.com/AxiomCore/AxiomCore/releases/download/0.1.93/axiom-macos-arm64.tar.gz"
+  sha256 "89a75d701539e12428003f3ce1e8c3b38d561530255883c49d3b3174cf804f05"
+  version "0.1.93"
 
   def install
     bin.install "axiom"
